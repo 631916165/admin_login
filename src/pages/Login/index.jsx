67a069f3f932f0bffac6a5cea9faeebb73756ca6
631916165 <631916165@qq.com>
2023-07-19 14:21:11 +0800

    登录功能开始

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,16 @@
 import React,{Component} from "react";
-import {Button} from 'antd'
-
+import { Card} from 'antd';
+import './index.css'
 
 export default class Login extends Component {
     render() {
         return(
-            <div>
-                <Button type="primary">登录</Button>
+            <div className="login">
+                <Card className="login-container"> 
+                <div className="title-login">
+                    <h2>后台管理系统</h2>
+                </div>
+                </Card>
             </div>
         )
     }
