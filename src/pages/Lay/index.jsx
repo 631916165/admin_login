7d30da7a0cca90a6162b48975cd75fd724bf5df2
631916165 <631916165@qq.com>
2023-07-20 17:18:04 +0800

    出了些小问题

--- src/pages/Lay/index.jsx
+++ src/pages/Lay/index.jsx
@@ -2,7 +2,7 @@
  * @Author: '631916165' '631916165@qq.com'
  * @Date: 2023-07-19 11:12:10
  * @LastEditors: '631916165' '631916165@qq.com'
- * @LastEditTime: 2023-07-20 17:03:55
+ * @LastEditTime: 2023-07-20 17:17:56
  * @FilePath: \admin_login\src\pages\Layout\index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
